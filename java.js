982274f53365852ae936626a1a2c1359049be800
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2022-10-25 09:20:02 -0600

    I have created the function to play against the computer and make the playerselection parameter case insensitive

--- java.js
+++ java.js
@@ -1,6 +1,32 @@
 const items = ["Rock", "Paper", "Scissors"];
-
+let playerSelection = ("RoCK").toLowerCase();
 
 function getComputerChoice(items) {
     return items[Math.floor(Math.random() * items.length)]; 
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "Scissors"){
+        return "You win, excellent!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "Paper"){
+        return "You lose! Paper beats Rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "Rock"){
+        return "You win, excellent!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "Scissors"){
+        return "You lose! Scissors beats Paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "Paper"){
+        return "You win, excellent!";
+    } 
+    else if (playerSelection === "scissors" && computerSelection === "Rock"){
+        return "You lose! Rock beats Scissors";
+    } 
+    else {
+       return "It's a draw =/";
+    }
+}
+
+
