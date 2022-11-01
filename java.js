84c65cb6d23e9f081883b00f8bdfce1c0e41a0a5
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2022-11-01 10:28:23 -0700

    I have created the last part where a function play five times the game and declared a winner, I was struggling with this part because I didn't know how to change the value of the variables to play different games.

--- java.js
+++ java.js
@@ -1,27 +1,37 @@
-const items = ["Rock", "Paper", "Scissors"];
-let playerSelection = ("RoCK").toLowerCase();
 
-function getComputerChoice(items) {
-    return items[Math.floor(Math.random() * items.length)]; 
-}
+let playerScore = 0;
+let computerScore = 0;
 
+function getComputerChoice() {
+    const items = ["rock", "paper", "scissors"];
+    const random = items[Math.floor(Math.random() * items.length)];
+     return random;
+    }
+    
+    
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === "rock" && computerSelection === "Scissors"){
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+        playerScore++;
         return "You win, excellent!";
     }
-    else if (playerSelection === "rock" && computerSelection === "Paper"){
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        computerScore++;
         return "You lose! Paper beats Rock";
     }
-    else if (playerSelection === "paper" && computerSelection === "Rock"){
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        playerScore++;
         return "You win, excellent!";
     }
-    else if (playerSelection === "paper" && computerSelection === "Scissors"){
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        computerScore++;
         return "You lose! Scissors beats Paper";
     }
-    else if (playerSelection === "scissors" && computerSelection === "Paper"){
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore++;
         return "You win, excellent!";
     } 
-    else if (playerSelection === "scissors" && computerSelection === "Rock"){
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        computerScore++;
         return "You lose! Rock beats Scissors";
     } 
     else {
@@ -29,4 +39,22 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+function game(){
+    for (let i = 0; i < 5; i++){
+        const playerSelection = prompt("Choose your weapon: ").toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
 
+    if (playerScore > computerScore){
+        return "You have won this war!";
+    }
+    else if ( playerScore < computerScore){
+        return "The computer is the victor!";
+    }
+    else{
+        return "It's a draw, try it again!";
+    }
+}
+
+console.log(game());
