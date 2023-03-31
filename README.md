47fb39220e0d65a7dcd579cb21e949427bab7c28
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-03-31 11:29:53 -0700

    I have modify my code to add an UI to the game and everything is working fine

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Project-Rock-Paper-Scissors
-I am going to create the old game rock papers scissors.
+I am going to create the old game rock papers scissors. I have added a UI to the game to be more intuitive.
+
+Credits for the creators of the photos:
+the rock photo
+Pixabay 
+http://pixabay.com
+
+the paper photo
+icon0.com 
+
+the scissors photo
+Karolina Grabowska
+instagram: kaboompics
+http://kaboompics.com
--- java.js
+++ java.js
@@ -1,13 +1,23 @@
 
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection = '';
+let computerSelection = getComputerChoice();
+let result = document.querySelector('.result');
+result.textContent = '';
+let showPlayerPoints = document.querySelector('.playerScore');
+showPlayerPoints.textContent = 'Player';
+let showComputerPoints = document.querySelector('.computerScore');
+showComputerPoints.textContent = 'Computer';
+let comments = document.querySelector('.comments');
+comments.textContent = 'Good luck!';
+let gameOver = false;
 
 function getComputerChoice() {
     const items = ["rock", "paper", "scissors"];
     const random = items[Math.floor(Math.random() * items.length)];
      return random;
     }
-    
     
 function playRound(playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "scissors"){
@@ -34,27 +44,91 @@ function playRound(playerSelection, computerSelection){
         computerScore++;
         return "You lose! Rock beats Scissors";
     } 
-    else {
+    else if (playerSelection === computerSelection){
        return "It's a draw =/";
     }
+    else{ return 'Error'}
 }
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Choose your weapon: ").toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
+const buttons = document.querySelectorAll('img');
+buttons.forEach((button) =>{
+    button.addEventListener('click', function  game(){
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+        if (gameOver === false){
+        result.textContent = playRound(playerSelection, computerSelection);}
 
-    if (playerScore > computerScore){
-        return "You have won this war!";
-    }
-    else if ( playerScore < computerScore){
-        return "The computer is the victor!";
-    }
-    else{
-        return "It's a draw, try it again!";
-    }
+        showPlayerPoints.textContent = 'Player ' + playerScore;
+        showComputerPoints.textContent = 'Computer ' + computerScore;
+
+        if(result.textContent === "You win, excellent!"){
+            switch(playerScore){
+                case 1:
+                    comments.textContent = 'Well done you have won the first round';
+                    break;
+                case 2:
+                    comments.textContent = 'Nice keep it this way';
+                    break;
+                case 3:
+                    comments.textContent = 'You Can do this =)';
+                    break;
+                case 4:
+                    comments.textContent = 'One more and you win this battle';
+                    break;
+                case 5:
+                    openPopup();
+                    document.querySelector('.winner').textContent = 'Well done you beat the computer ;D';
+                    endGame()
+                    break;
+                }
+        } 
+        else if(result.textContent === "You lose! Paper beats Rock" || result.textContent === "You lose! Scissors beats Paper" 
+        || result.textContent === "You lose! Rock beats Scissors"){
+            switch(computerScore){
+                case 1:
+                    comments.textContent = "A simple mistake, don't worry";
+                    break;
+                case 2:
+                    comments.textContent = "It's just 2 points keep fighting";
+                    break;
+                case 3:
+                    comments.textContent = 'Be careful the computer have 3 points';
+                    break;
+                case 4:
+                    comments.textContent = 'Warning 1 more point and you will lose';
+                    break;
+                case 5:
+                    openPopup();
+                    document.querySelector('.winner').textContent = 'You were defeat by the computer =(';
+                    endGame()
+                    break;
+            }
+        }
+      });
+});
+
+// this part show a message when someone get the 5 points
+let popup = document.getElementById('popup');
+
+function openPopup(){
+    popup.classList.add('open-popup');
 }
 
-console.log(game());
+function endGame(){
+    gameOver = true;
+}
+
+//code to restart the game 
+const restart = document.querySelector('.restart');
+restart.addEventListener('click', () =>{
+  popup.classList.remove('open-popup');
+  playerScore = 0;
+  computerScore = 0;
+  playerSelection = '';
+  computerSelection = getComputerChoice();
+  result.textContent = '';
+  showPlayerPoints.textContent = 'Player';
+  showComputerPoints.textContent = 'Computer';
+  comments.textContent = 'Good luck!';
+  gameOver = false;
+});
